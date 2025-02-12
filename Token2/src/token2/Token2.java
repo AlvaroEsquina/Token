a78a9a4f78cc23eeb973bc27f2699ea98b6078e6
AlexDiez <alex.diez1@educa.madrid.org>
2025-02-12 13:25:47 +0100

    v4 sr

--- Token2/src/token2/Token2.java
+++ Token2/src/token2/Token2.java
@@ -15,6 +15,83 @@ public class Token2 {
      */
     public static void main(String[] args) {
         System.out.println("Culona como Sophie Rain y como tu mare 😁");
+        while (true){
+            System.out.println("Jaja, ey\n" +
+"\n" +
+"Solo estamo' horny, ey, no love\n" +
+"Ella por la morning le llegó\n" +
+"A empezar el día me ayudó\n" +
+"La sacó del frasco y lo enroló\n" +
+"\n" +
+"El celu sonando y lo ignoró\n" +
+"Él se lo imagina, él no es morón\n" +
+"Ninguno se podía envolver\n" +
+"Pero le metí y se enamoró\n" +
+"\n" +
+"Culona como Sophie Rain\n" +
+"Y en los día' lluvioso' le da con chingar, eso es por ley\n" +
+"Si me necesita', yo te puedo socorrer\n" +
+"Ropa de ejercicio, ella dijo que iba a correr\n" +
+"\n" +
+"Solo estamo' horny, ey, no love\n" +
+"Ella por la morning le llegó\n" +
+"A empezar el día me ayudó\n" +
+"La sacó del frasco y lo enroló\n" +
+"\n" +
+"El celu sonando y lo ignoró\n" +
+"Él se lo imagina, él no es morón\n" +
+"Ninguno se podía envolver\n" +
+"Pero le metí y se enamoró\n" +
+"\n" +
+"Ella no sube na' de lo nuestro aunque sea influencer\n" +
+"Nunca me niega que hay vario' en fila, le dan terapia, pero no la convencen\n" +
+"Yo te juro que ya extraño hacerlo como aquella' vece'\n" +
+"Si en algún momento por mí te preguntan, no le des luz, dile que soy tú best friend\n" +
+"\n" +
+"De na' má' que yo la bese se le moja el panty, eso se le humedece\n" +
+"Prepotente por la calle, pero en la cama ella siempre obedece\n" +
+"Deja que él siga en su viaje y que se la crea sola, eso le aborrece\n" +
+"Piensa que estoy misionando y ando con Quality grabando las refe\n" +
+"\n" +
+"Solo estamo' horny, ey, no love\n" +
+"Fue al baño con la amiga a la ve' y sacaron un polvo de color\n" +
+"Ellas saben que la tengo en play, que no me va bien, me va cabrón\n" +
+"Me vio de frente y me abacoró, se puso en cuatro y se lo atoró\n" +
+"Young Kingz, baby, ey\n" +
+"\n" +
+"Solo estamo' horny, ey, no love\n" +
+"Ella por la morning le llegó\n" +
+"A empezar el día me ayudó\n" +
+"La sacó del frasco y lo enroló\n" +
+"\n" +
+"El celu sonando y lo ignoró\n" +
+"Él se lo imagina, él no es morón\n" +
+"Ninguno se podía envolver\n" +
+"Pero le metí y se enamoró\n" +
+"\n" +
+"Culona como Sophie Rain\n" +
+"Y en los día' lluvioso' le da con chingar, eso es por ley\n" +
+"Si me necesita', yo te puedo socorrer\n" +
+"Ropa de ejercicio, ella dijo que iba a correr\n" +
+"\n" +
+"Solo estamo' horny, ey, no love\n" +
+"Ella por la morning le llegó\n" +
+"A empezar el día me ayudó\n" +
+"La sacó del frasco y lo enroló\n" +
+"\n" +
+"El celu sonando y lo ignoró\n" +
+"Él se lo imagina, él no es morón\n" +
+"Ninguno se podía envolver\n" +
+"Pero le metí y se enamoró\n" +
+"\n" +
+"Jejeje\n" +
+"¿Que tú quiere' que yo haga si te envolviste a la primera?\n" +
+"Young Kingz, baby\n" +
+"Y bien bellaca está diciendo que quiere a uno y to'\n" +
+"Ah, ah\n" +
+"¿Qué hora es, mi amor?");
+        }
+            
     }
     
 }
